420933efde6632c8a1398733ebe2a244133f6d61
masaniki <masaniki.software@gmail.com>
2026-03-19 17:34:56 +0900

    end: batによるtestに変更する。

---
+++ tests/test_invalid_anoy.py
@@ -0,0 +1,43 @@
+import pytest
+from pathlib import Path
+import sys
+import yaml
+import difflib
+
+import logging
+import logging.config
+
+#sys.pathを弄る。
+PROJECT_DIR=Path(__file__).parent.parent
+sys.path.append(str(PROJECT_DIR))
+
+from src.anoy import AnoyParser
+from src.anoy import ConfigYamlError
+
+TESTS_DIR=Path(__file__).parent
+
+def test_invalid_anoy():
+    """
+    @Summ: configが正常∧anoyが異常な場合をtestする。
+
+    @Desc:
+    - testに必要なfileは全てtestsuite directoryに格納する。
+
+    """
+    # logginの環境設定。
+    suitePath=TESTS_DIR/"invalid_anoy"/"testsuite"
+    loggingConfigPath=suitePath/"logging_config.yaml"
+    with open(loggingConfigPath,mode="r",encoding="utf-8") as f:
+        configDict=yaml.safe_load(f)
+    logging.config.dictConfig(configDict)
+    inputPath=suitePath/"in"
+    for childPath in inputPath.iterdir():
+        anoyPath=childPath/"anoy.yaml"
+        configPath=childPath/"config.yaml"
+        with open(configPath,mode="r",encoding="utf-8") as f:
+            configDict=yaml.safe_load(f)
+        tree01=AnoyParser(configDict)
+        tree01.dirDFS(anoyPath)
+
+if(__name__=="__main__"):
+    test_invalid_anoy()
